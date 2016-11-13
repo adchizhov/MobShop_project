cc4b80f5df1c736e925ead0424064918c17582ef
adchizhov <adchizhov@mail.ru>
2016-11-13 14:22:42 +0300

    added smth

--- eshop_project/eshop/views.py
+++ eshop_project/eshop/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+# from .models import 
+from django.utils import timezone
 
 # Create your views here.
+
+def index_page(request):
+    if request.method == "GET":
+        return HttpResponse('Welcome to nothing')
+
