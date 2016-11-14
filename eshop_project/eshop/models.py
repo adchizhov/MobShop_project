0427d1faeab3468a4b434991cf282cd537aea5cc
Alexander Chizhov <adchizhov@mail.ru>
2016-11-14 15:33:03 +0300

    added first models

--- eshop_project/eshop/models.py
+++ eshop_project/eshop/models.py
@@ -1,5 +1,28 @@
-from __future__ import unicode_literals
-
+import datetime
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible # support python 2
 
-# Create your models here.
+
+@python_2_unicode_compatible
+class Product(models.Model):
+    sku = models.CharField(max_length=15)
+    name = models.CharField(max_length=30)
+    category = models.CharField(max_length=30)
+    price = models.DecimalField(max_digits=100, decimal_places=2)
+    description = models.CharField(max_length=255)
+
+    def __str__(self):
+        return ('Product: {}, description: {}, price {}'.format(self.name, self.description, self.price))
+
+@python_2_unicode_compatible
+class Manufacturer(models.Model):
+    product_name = models.ForeignKey(Product, on_delete=models.CASCADE)
+    manufacturer_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return ('Manufacturer: {}'.format(self.manufacturer_name))
+
+
+@python_2_unicode_compatible
+class Order(models.Model):
+	pass
