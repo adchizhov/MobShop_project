96ba7b0d2a8587fb311100841426e7f7efb45a27
Alexander Chizhov <adchizhov@mail.ru>
2016-11-15 11:30:48 +0300

    little changes in models and filled Customer, Order

--- eshop_project/eshop/models.py
+++ eshop_project/eshop/models.py
@@ -4,26 +4,26 @@ from django.utils.encoding import python_2_unicode_compatible # поддержи
 
 
 @python_2_unicode_compatible
-class Product(models.Model):
-    sku = models.CharField(max_length=15)
-    manufacturer = models.OneToOneField(Manufacturer, on_delete=models.CASCADE)
-    name = models.CharField(max_length=30)
-    category = models.CharField(max_length=30)
-    price = models.DecimalField(max_digits=100, decimal_places=2)
-    description = models.CharField(max_length=250)
-    stock = models.PositiveSmallIntegerField()
-    in_stock = models.BooleanField(default=True)
-
-    def __str__(self):
-        return ('Product: {}, description: {}, price {}'.format(self.name, self.description, self.price))
-
-
-@python_2_unicode_compatible
 class Manufacturer(models.Model):
     manufacturer_name = models.CharField(max_length=50)
 
     def __str__(self):
         return ('Manufacturer: {}'.format(self.manufacturer_name))
+
+
+@python_2_unicode_compatible
+class Product(models.Model):
+    sku = models.CharField(max_length=15)
+    manufacturer = models.ForeignKey('Manufacturer', on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    category = models.CharField(max_length=30)
+    price = models.DecimalField(max_digits=100, decimal_places=2)
+    description = models.CharField(max_length=250)
+    stock = models.PositiveSmallIntegerField(default=1)
+    in_stock = models.BooleanField(default=True)
+
+    def __str__(self):
+        return ('Product: {}, description: {}, price {}'.format(self.name, self.description, self.price))
 
 
 @python_2_unicode_compatible
@@ -39,10 +39,11 @@ class Customer(models.Model):
 
 @python_2_unicode_compatible
 class Order(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product)
+    customer = models.ForeignKey('Customer', on_delete=models.CASCADE)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
     ordered_datetime = models.DateTimeField()
     comment = models.CharField(max_length=140, blank=True)
+
     def __str__(self):
         return ('Customer: {} ordered {} at {} with comment {}'.format(
         	self.customer, self.product, self.ordered_datetime, self.comment
