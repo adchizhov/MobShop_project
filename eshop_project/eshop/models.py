1ee39b1e4cbfd6598125c07c34e6de98054c338d
Alexander Chizhov <adchizhov@mail.ru>
2016-11-18 11:04:08 +0300

    added phonemodel, choices and etc

--- eshop_project/eshop/models.py
+++ eshop_project/eshop/models.py
@@ -1,5 +1,6 @@
 import datetime
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 from django.utils.encoding import python_2_unicode_compatible # поддержим python 2
 
 
@@ -10,13 +11,25 @@ class Manufacturer(models.Model):
     def __str__(self):
         return ('Manufacturer: {}'.format(self.manufacturer_name))
 
+@python_2_unicode_compatible
+class PhoneVersion(models.Model):
+    gb_16 = ('16 gb', '16 gb version')
+    gb_32 = ('32 gb', '32 gb version')
+    gb_64 = ('64 gb', '64 gb version')
+    gb_128 = ('128 gb', '128 gb version')
+    __all = (gb_16, gb_32, gb_64, gb_128)
+    version = models.CharField(max_length=5, choices=__all)
+
+    def __str__(self):
+        return self.version
 
 @python_2_unicode_compatible
-class Product(models.Model):
+class PhoneProduct(models.Model):
     sku = models.CharField(max_length=15)
     manufacturer = models.ForeignKey('Manufacturer', on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
     category = models.CharField(max_length=30)
+    version = models.ForeignKey('PhoneVersion')
     price = models.DecimalField(max_digits=100, decimal_places=2)
     description = models.CharField(max_length=250)
     stock = models.PositiveSmallIntegerField(default=1)
@@ -25,13 +38,20 @@ class Product(models.Model):
     def __str__(self):
         return ('Product: {}, description: {}, price {}'.format(self.name, self.description, self.price))
 
+@python_2_unicode_compatible
+class Address(models.Model):
+    full = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.full
 
 @python_2_unicode_compatible
 class Customer(models.Model):
     first_name = models.CharField(max_length=15)
     last_name = models.CharField(max_length=20)
     email = models.EmailField()
-    adress = models.CharField(max_length=60)
+    phone_number = PhoneNumberField(default='', unique=True)
+    address = models.ForeignKey('Address')
     
     def __str__(self):
         return ('Customer: {} {}'.format(self.first_name, self.last_name))
@@ -39,15 +59,15 @@ class Customer(models.Model):
 
 @python_2_unicode_compatible
 class Order(models.Model):
-	# стойкое ощущение что здесь многого не хватает
+    # стойкое ощущение что здесь многого не хватает
     customer = models.ForeignKey('Customer', on_delete=models.CASCADE)
-    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    product = models.ForeignKey('PhoneProduct', on_delete=models.CASCADE)
     ordered_datetime = models.DateTimeField()
     comment = models.CharField(max_length=140, blank=True)
 
     def __str__(self):
         return ('Customer: {} ordered {} at {} with comment {}'.format(
-        	self.customer, self.product, self.ordered_datetime, self.comment
-        	))
+            self.customer, self.product, self.ordered_datetime, self.comment
+            ))
 
 
