6d4bc4b127e1eb9bdb5df6c03fe82986d42107d2
Alexander Chizhov <adchizhov@mail.ru>
2016-11-18 11:24:27 +0300

    register models in admin

--- eshop_project/eshop/admin.py
+++ eshop_project/eshop/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(Manufacturer,
+	PhoneVersion,
+	PhoneProduct,
+	Address,
+	Customer,
+	Order,
+	)
