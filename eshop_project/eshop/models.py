121e4f8bc07d8e76bf37604e2ecd41d7c64ce4bb
Alexander Chizhov <adchizhov@mail.ru>
2016-11-24 12:00:12 +0300

    added fixtures

--- eshop_project/eshop/models.py
+++ eshop_project/eshop/models.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+from __future__ import unicode_literals
 import datetime
 import moneyed
 from djmoney.models.fields import MoneyField
--- eshop_project/eshop/views.py
+++ eshop_project/eshop/views.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 # from .models import 
