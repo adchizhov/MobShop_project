610de8a39d9666ddff93475f897d2457e63fd55d
Alexander Chizhov <adchizhov@mail.ru>
2016-11-24 12:07:20 +0300

    small

--- eshop_project/eshop/admin.py
+++ eshop_project/eshop/admin.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.contrib import admin
-# Register your models here.
 from .models import *
-
+# Register your models here.
 
 class CustomManufacturer(admin.ModelAdmin):
     list_display = ('manufacturer_name',)
