fd6be0cb879cd2b8b1211a3589cdb209da469184
adchizhov <adchizhov@mail.ru>
2016-12-13 20:38:22 +0300

    changes in settings

--- eshop_project/eshop_project/settings.py
+++ eshop_project/eshop_project/settings.py
@@ -187,7 +187,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
