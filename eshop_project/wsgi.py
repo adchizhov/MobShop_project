0cf0b552666c56fd18d6c7a0c3cf2a4059afdd0f
adchizhov <adchizhov@mail.ru>
2016-12-14 11:46:31 +0300

    still trying to deploy

--- eshop_project/wsgi.py
+++ eshop_project/wsgi.py
@@ -10,9 +10,10 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eshop_project.settings")
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
 application = DjangoWhiteNoise(application)
